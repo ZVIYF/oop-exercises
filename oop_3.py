4d56606999a164cdf9a129726484022482449db3
צבי פישר <zviyf132@gmail.com>
2026-06-24 12:27:33 +0300

    oop_3 finish

--- oop_3.py
+++ oop_3.py
@@ -1,42 +1,146 @@
 # # # # # # # # # # # # # #
 
-class Vehicle:
-    pass
+# class Vehicle:
+#     pass
+#
+# class Engine:
+#     pass
+#
+# class Doors:
+#     pass
+#
+# class Wings:
+#     pass
+#
+# class Motorcycle(Vehicle):
+#     def __init__(self):
+#         super().__init__()
+#         self.engine = Engine
+#
+# class Car(Vehicle):
+#     def __init__(self):
+#         super().__init__()
+#         self.engine = Engine
+#         self.doors = Doors
+#
+# class Bicycle(Vehicle):
+#     def __init__(self):
+#         super().__init__()
+#
+# class Airplane:
+#     def __init__(self):
+#         self.engine = Engine
+#         self.doors = Doors
+#         self.wings = Wings
+#
+# class Electric_scooter(Vehicle):
+#     def __init__(self):
+#         super().__init__()
+#         self.engine = Engine
+#
+# class Boat:
+#     def __init__(self):
+#         self.engine = Engine
 
-class Engine:
-    pass
-
-class Doors:
-    pass
-
-class Wings:
-    pass
-
-class Motorcicle(Vehicle):
-    def __init__(self):
-        super().__init__()
-        self.engine = Engine
-
-class Car(Vehicle):
-    def __init__(self):
-        super().__init__()
-        self.engine = Engine
-        self.doors = Doors
-
-class Bicycle(Vehicle):
-    def __init__(self):
-        super().__init__()
-
-class Airplane:
-    def __init__(self):
-        self.engine = Engine
-        self.doors = Doors
-        self.wings = Wings
-
-class Electric_scooter(Vehicle):
-    def __init__(self):
-        super().__init__()
-        self.engine = Engine
 
 # # # # # # # # # # # # # # # #
 
+# class Animal:
+#     pass
+#
+# class Mammal(Animal):
+#     pass
+#
+# class Legs:
+#     pass
+#
+# class Wings:
+#     pass
+#
+# class Fins:
+#     pass
+#
+# class Tail:
+#     pass
+#
+# class Fish(Animal):
+#     def __init__(self):
+#         super().__init__()
+#         self.fins = Fins
+#         self.tail = Tail
+#
+# class Bird(Animal):
+#     def __init__(self):
+#         super().__init__()
+#         self.tail = Tail
+#         self.wings = Wings
+#         self.legs = Legs
+#
+# class Cat(Mammal):
+#     def __init__(self):
+#         super().__init__()
+#         self.tail = Tail
+#         self.legs = Legs
+#
+# class Dog(Mammal):
+#     def __init__(self):
+#         super().__init__()
+#         self.tail = Tail
+#         self.legs = Legs
+#
+# class Spider(Animal):
+#     def __init__(self):
+#         super().__init__()
+#         self.legs = Legs
+
+# # # # # # # # # # # # # # #
+
+# class ElectronicDevice:
+#     pass
+#
+# class Camera(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#         self.screen = None
+#
+# class Computer(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#         self.screen = Screen
+#         self.speakers = Speakers
+#         self.keyboard = Keyboard
+#         self.mouse = Mouse
+#
+# class Speakers(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#
+# class Keyboard(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#
+# class Television(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#         self.screen = Screen
+#         self.speakers = Speakers
+#
+# class Screen(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#
+# class Mouse(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#
+# class MobilePhone(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#         self.screen = Screen
+#         self.speakers = Speakers
+#
+# class Printer(ElectronicDevice):
+#     def __init__(self):
+#         super().__init__()
+#         self.speakers = Speakers
+#         self.screen = None
