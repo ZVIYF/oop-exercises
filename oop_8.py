4fafc58b3ac103192a7dacb9247dd99721f8e85a
צבי פישר <zviyf132@gmail.com>
2026-06-25 12:33:05 +0300

    User class, oop_8

--- oop_8.py
+++ oop_8.py
@@ -1,3 +1,6 @@
+from curses.ascii import isdigit
+
+
 class BankAccount:
     def __init__(self, account_number, initial_balance = 0):
         self.__account_number = account_number
@@ -46,13 +49,13 @@ class Person:
 
     @name.setter
     def name(self, name):
-        if isinstance(name, str):
-            if name.strip():
-                self.__name = name
-            else:
-                print("Error: Name can't be empty!")
-        else:
+        if not isinstance(name, str):
             print("Error: Name has to be a string!")
+            return
+        if not name.strip():
+            print("Error: Name can't be empty!")
+            return
+        self.__name = name
 
     @property
     def age(self):
@@ -82,3 +85,58 @@ class Person:
 # haim.age += 12
 # print(haim.age)
 # print(haim.category)
+
+# # # # # # # # # # # # # # #
+
+class User:
+    __user_count = 0
+    __users_list = []
+
+    def __init__(self, username, e_mail, password):
+        self.username = username
+        self.email = e_mail
+        self.__password_hash = password.__hash_password()
+        User.__user_count += 1
+        User.__users_list.append(self)
+
+    @staticmethod
+    def __hash_password(former_password):
+        return str(hash(former_password))
+
+    @staticmethod
+    def is_valid_email(email):
+        if isinstance(email, str):
+            if "@" in email:
+                return True
+        return False
+
+    @staticmethod
+    def is_strong_password(my_pass):
+        if len(my_pass) < 8:
+            return False
+        has_upper, has_lower, has_digits = False
+        for char in my_pass: # הקוד לא לוקח בחשבון תווים מיוחדים?
+            if isdigit(char):
+                has_digits = True
+            elif char == char.upper:
+                has_upper = True
+            elif char == char.lower:
+                has_lower = True
+        return has_lower and has_upper and has_digits
+
+    @staticmethod
+    def creat_user_safely(username, email, password):
+        if not User.is_valid_email(email):
+            print("Error: Invalid E-mail!"
+                  "Please try again...")
+            return None
+        if not User.is_strong_password(password):
+            print("Error: Invalid password.\n"
+                  "Password must have:\n"
+                  " - at least 8 characters.\n"
+                  " - at least 1 upper case.\n"
+                  " - at least 1 lower case.\n"
+                  " - at least 1 digit.\n"
+                  "Please try again.")
+            return None
+        return User(username, email, password)
